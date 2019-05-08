adcbcb5aac6706eca6f876cb9735f0a8c874f7bb
PARKHONGSOON <kongsooni39@gmail.com>
2019-05-09 00:02:31 +0900

    Add Project, Initial Commit

---
+++ hong-sb/src/main/java/com/soon/hongsb/HongSbApplication.java
@@ -0,0 +1,13 @@
+package com.soon.hongsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongSbApplication.class, args);
+    }
+
+}
---
+++ hong-sb/src/test/java/com/soon/hongsb/HongSbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soon.hongsb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HongSbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
